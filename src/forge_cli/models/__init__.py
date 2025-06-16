7da3b4521841cc8eeeab134395a9f077f41cd80d
Junix <juns1984@qq.com>
2025-06-16 22:45:07 +0800

    refactor: Remove state management models to simplify the codebase

--- src/forge_cli/models/__init__.py
+++ src/forge_cli/models/__init__.py
@@ -1,13 +1,8 @@
 """Model definitions for the refactored file search module."""
 
 from .conversation import ConversationState
-from .state import StreamState, ToolState, ToolStatus
 
 __all__ = [
-    # State
-    "StreamState",
-    "ToolState",
-    "ToolStatus",
     # Conversation
     "ConversationState",
 ]
