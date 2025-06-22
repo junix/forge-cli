c518215321d347ab068a6f92882b78ac98e9065a
junix <juns1984@qq.com>
2025-06-22 14:04:49 +0800

    fix

---
+++ src/forge_cli/display/v3/renderers/rendable.py
@@ -0,0 +1,57 @@
+from typing import Protocol
+
+from rich.align import Align
+from rich.columns import Columns
+from rich.console import Group
+from rich.json import JSON
+from rich.layout import Layout
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich.progress import Progress
+from rich.rule import Rule
+from rich.syntax import Syntax
+from rich.table import Table
+from rich.text import Text
+from rich.tree import Tree
+
+
+class Rendable(Protocol):
+    """Protocol for rendable objects."""
+
+    def render(
+        self, **kwargs
+    ) -> (
+        str
+        | Text
+        | Panel
+        | Table
+        | JSON
+        | Markdown
+        | Syntax
+        | Tree
+        | Rule
+        | Align
+        | Columns
+        | Group
+        | Layout
+        | Progress
+        | list[
+            str
+            | Text
+            | Panel
+            | JSON
+            | Table
+            | Markdown
+            | Syntax
+            | Tree
+            | Rule
+            | Align
+            | Columns
+            | Group
+            | Layout
+            | Progress
+        ]
+        | None
+    ):
+        """Render the object."""
+        ...
